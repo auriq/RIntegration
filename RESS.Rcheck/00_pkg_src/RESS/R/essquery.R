f55f09839060457bc1624dd46eb6ba964dd62c2e
EC2 Default User <ec2-user@ip-10-0-0-44.ec2.internal>
2015-07-23 19:01:18 +0000

    RESS 1.2, shortened ess3.0.7 commands, closed connections

--- RESS.Rcheck/00_pkg_src/RESS/R/essquery.R
+++ RESS.Rcheck/00_pkg_src/RESS/R/essquery.R
@@ -6,7 +6,7 @@ essQuery <- function(essentia, aq = "", flags = "") {
       essentia <- "ess exec"
     }
     if (((substr(essentia, 1, 8) == "ess exec") && (!grepl("#Rignore", flags))) || ((substr(essentia, 1, 10) == "ess stream" || substr(essentia, 1, 9) == "ess query") && (grepl("#Rinclude", flags)))) {
-      if (substr(essentia, 1, 3) == "ess") {
+      if ((substr(essentia, 1, 10) == "ess stream") || (substr(essentia, 1, 8) == "ess exec")) {
 	      aq <- paste(aq, "; echo 'RSTOPHERE'", sep = "")
       }
       else { flags <- paste("; echo 'RSTOPHERE'", flags, sep=" ") }
@@ -21,7 +21,9 @@ essQuery <- function(essentia, aq = "", flags = "") {
         varname <- substr(line, titleindex[[1]] + 3, titleindex[[2]] - 1)
         remove(titleindex)
       }
-      t2 <- read.csv(pipe(line, open = "r"), header = colspec, sep = ",", quote = "\"'", comment.char = "#", blank.lines.skip = FALSE, allowEscapes = TRUE, skip = 0)
+      pipedline <- pipe(line, open = "r")
+      t2 <- read.csv(pipedline, header = colspec, sep = ",", quote = "\"'", comment.char = "#", blank.lines.skip = FALSE, allowEscapes = TRUE, skip = 0)
+      close(pipedline)
       index <- 1
       t3 <- NULL
       separate <- grepl(" #Rseparate", line)
