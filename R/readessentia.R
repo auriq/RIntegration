3ddf2abd935457f6c324f48458aee66b4f90a54d
Ben Waxer <bwaxer@auriq.com>
2015-10-26 23:42:07 +0000

    updated for read.essentia with notitle, tested WITHOUT cran

--- R/readessentia.R
+++ R/readessentia.R
@@ -1,8 +1,13 @@
 read.essentia <- function(file) {
   pipedline <- pipe(sprintf("bash %s",file), open = "r")
-  t2 <- read.csv(pipedline, header = TRUE, sep = ",", 
+  colspec <- TRUE
+  if (grepl("-notitle", file)) {
+    colspec <- FALSE
+  }
+  t2 <- read.csv(pipedline, header = colspec, sep = ",", 
                  quote = "\"'", comment.char = "#", blank.lines.skip = FALSE, 
                  allowEscapes = TRUE, skip = 0)
+  rm(colspec)
   close(pipedline)
   return(t2)
 }
